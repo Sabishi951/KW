35d05ca675fae9af5c5a4c85a07f9ff6a7a2834c
Sabishi951 <104005901+Sabishi951@users.noreply.github.com>
2024-12-18 20:22:26 +0100

    Delete ballManager.js

--- ballManager.js
+++
@@ -1,9 +0,0 @@
-class ballManager {
-    constructor() {
-        const kulkaReset = new ballReset();
-        const kulkaExp = new ballExp();
-        const kulkaUpgrade = new ballUpgrade();
-        const petReset = new pet_bonch();
-        const anielskaKulka = new anielskaReset();
-    }
-}
